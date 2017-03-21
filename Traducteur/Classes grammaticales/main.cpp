6ca8eb1480d8b181eb7f8f0bb8a8ed6dc0fea54e
Gregoire <greg-marco@hotmail.fr>
2017-03-21 11:17:45 +0100

    Corrections.

---
+++ Traducteur/Classes grammaticales/main.cpp
@@ -0,0 +1,21 @@
+//
+//  main.cpp
+//  Traducteur_universel (Version 1.1.0 - Hydrogène)
+//
+//  Created by Grégoire on 13/09/2015.
+//  Copyright (c) 2015 Grégoire. All rights reserved.
+//
+
+#include "Fenetre.hpp"
+
+using namespace std;
+
+
+int main(int argc, const char * argv[])
+{
+    Fenetre tryba;
+    
+    tryba.creation_de_la_fenetre();
+    
+    return 0;
+}
